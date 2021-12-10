015b20c545cfb5ae1d47ac9a10af3d7d2e38a8df
Nathalie Agthe <agthnezy@student.jyu.fi>
2021-12-10 11:38:37 +0200

    full stack

--- models/person.js
+++ models/person.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+//mongoose.set('useFindAndModify', false)
+//mongoose.set('useCreateIndex', true)
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
+
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
@@ -12,9 +17,21 @@ mongoose.connect(url)
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    }, 
+    number: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 8
+    }
   })
+
+  personSchema.plugin(uniqueValidator)
 
   personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
